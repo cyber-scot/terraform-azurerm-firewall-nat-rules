a23cf94e6ed13e20efde1dbd487856b5336c620b
Craig Thacker <craigthackerx@gmail.com>
2023-10-22 15:09:31 +0100

    Update module

--- main.tf
+++ main.tf
@@ -11,11 +11,14 @@ resource "azurerm_firewall_nat_rule_collection" "nat_rules" {
     for_each = each.value.rules
     content {
       name                  = rule.value.name
-      protocols             = rule.value.protocols
+      protocols             = upper(rule.value.protocols)
+      description           = rule.value.description
       source_addresses      = rule.value.source_addresses
+      source_ip_groups      = rule.value.source_ip_groups
       destination_addresses = rule.value.destination_addresses
       destination_ports     = rule.value.destination_ports
       translated_address    = rule.value.translated_address
+      translated_fqdn       = rule.value.translated_fqdn
       translated_port       = rule.value.translated_ports
     }
   }
